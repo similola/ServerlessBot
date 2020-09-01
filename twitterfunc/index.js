803a7afb2c802a75fc5422a96825316661fa47b9
siawokoy <similola-a@hotmail.com>
2020-09-01 10:41:39 +0100

    updating tweet

--- twitterfunc/index.js
+++ twitterfunc/index.js
@@ -2,7 +2,7 @@ module.exports = async function (context, req) {
     let currentdate = new Date();
     context.log('Generating Tweet...');
     context.res = {
-        body: "Your daily reminder " + currentdate.getDate() + "/"
+        body: "Your daily reminder is today " + currentdate.getDate() + "/"
             + (currentdate.getMonth() + 1) + "/"
             + currentdate.getFullYear() + " at "
             + currentdate.getHours() + ":"
