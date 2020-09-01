42b3837c3f6eed2a64cf8b42878f705c25fffb83
siawokoy <similola-a@hotmail.com>
2020-09-01 10:15:30 +0100

    updating tweet code

--- twitterfunc/index.js
+++ twitterfunc/index.js
@@ -1,16 +1,11 @@
 module.exports = async function (context, req) {
-    context.log('JavaScript HTTP trigger function processed a request.');
-
-    if (req.query.name || (req.body && req.body.name)) {
-        context.res = {
-            // status: 200, /* Defaults to 200 */
-            body: "Hello " + (req.query.name || req.body.name)
-        };
-    }
-    else {
-        context.res = {
-            status: 400,
-            body: "Please pass a name on the query string or in the request body"
-        };
-    }
+    let currentdate = new Date();
+    context.log('Generating Tweet...');
+    context.res = {
+        body: "Your daily reminder " + currentdate.getDate() + "/"
+            + (currentdate.getMonth() + 1) + "/"
+            + currentdate.getFullYear() + " at "
+            + currentdate.getHours() + ":"
+            + currentdate.getMinutes()
+    };
 };
